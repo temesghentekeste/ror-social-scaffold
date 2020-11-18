324c52f8130263a3b6ede81fd926ed1d1b10b762
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-18 12:46:01 +0300

    Add migration for adding unique index to friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :invitor, class_name: 'User'
+  validates :invitor, uniqueness: { scope: :invitee }
+
   belongs_to :invitee, class_name: 'User'
 end
