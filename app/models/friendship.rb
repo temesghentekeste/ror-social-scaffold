aac299919db9951f688b15344253a049c3ac0bdb
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-18 13:22:20 +0300

    Add two row mutual friendship using callbacks

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,21 @@ class Friendship < ApplicationRecord
   validates :invitor, uniqueness: { scope: :invitee }
 
   belongs_to :invitee, class_name: 'User'
+
+  after_update :create_reverse_friendship
+  after_destroy :destroy_reverse_friendship, if: :reverse_friendship_exists?
+
+  private
+
+    def create_reverse_friendship
+      Friendship.create(invitor_id: invitee_id, invitee_id: invitor_id, confirmed: true)
+    end
+
+    def destroy_reverse_friendship
+      Friendship.find_by(invitor_id: invitee_id, invitee_id: invitor_id, confirmed: true).destroy
+    end
+    
+    def reverse_friendship_exists?
+      true if Friendship.find_by(invitor_id: invitee_id, invitee_id: invitor_id, confirmed: true)
+    end
 end
