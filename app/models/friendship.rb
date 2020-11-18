bcceee0f7c03ec98c4132e64410627babaee53d8
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-11-18 15:05:03 +0300

    Update model validations and tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :invitor, class_name: 'User'
-  validates :invitor, uniqueness: { scope: :invitee }
+  # validates :invitor, uniqueness: { scope: :invitee }
+  validates_uniqueness_of :invitee, scope: :invitor
+  validates_uniqueness_of :invitor, scope: :invitee
 
   belongs_to :invitee, class_name: 'User'
 
@@ -20,4 +22,5 @@ class Friendship < ApplicationRecord
     def reverse_friendship_exists?
       true if Friendship.find_by(invitor_id: invitee_id, invitee_id: invitor_id, confirmed: true)
     end
+
 end
